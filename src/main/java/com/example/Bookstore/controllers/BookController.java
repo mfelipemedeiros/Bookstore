3eef3cabdb8e7d542c2523e7a9a0899b58822077
Matheus Felipe <65515159+mfelipemedeiros@users.noreply.github.com>
2024-02-01 15:02:45 -0300

    Find by title

--- src/main/java/com/example/Bookstore/controllers/BookController.java
+++ src/main/java/com/example/Bookstore/controllers/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,6 @@ import com.example.Bookstore.entities.BookEntity;
 import com.example.Bookstore.repositories.BookRepository;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -39,5 +39,10 @@ public class BookController {
 
         return ResponseEntity.status(HttpStatus.OK).body(bookList);
     }
-    
+        @GetMapping("/get/{title}")
+    public ResponseEntity<List<BookEntity>> getByName(@PathVariable(value="title" ) String title) {
+        List<BookEntity> bookList = bookRepository.findByTitle(title);
+
+        return ResponseEntity.status(HttpStatus.OK).body(bookList);
+    }
 }
--- src/main/java/com/example/Bookstore/repositories/BookRepository.java
+++ src/main/java/com/example/Bookstore/repositories/BookRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Bookstore.entities.BookEntity;
+import java.util.List;
+
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long>{
     
+    List<BookEntity> findByTitle(String title);
 }
