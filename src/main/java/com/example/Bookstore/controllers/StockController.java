d68ae8a74b97ca30b20463e6175e42039242cc5c
Matheus Felipe <65515159+mfelipemedeiros@users.noreply.github.com>
2024-02-16 19:03:34 -0300

    StockController and StockDto

--- src/main/java/com/example/Bookstore/controllers/StockController.java
+++ src/main/java/com/example/Bookstore/controllers/StockController.java
@@ -1,10 +1,22 @@
 package com.example.Bookstore.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Bookstore.dtos.StockDto;
+import com.example.Bookstore.entities.BookEntity;
+import com.example.Bookstore.entities.StockEntity;
 import com.example.Bookstore.repositories.StockRepository;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/bookstore/stock")
@@ -13,5 +25,11 @@ public class StockController {
     @Autowired
     StockRepository stockRepository;
 
-    
+    @PostMapping("/addItem")
+    public ResponseEntity<StockEntity> addItem(@RequestBody StockDto stockDto) {
+        var stockModel = new StockEntity();
+        BeanUtils.copyProperties(stockDto, stockModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(stockRepository.save(stockModel));
+    }
+        
 }
---
+++ src/main/java/com/example/Bookstore/dtos/StockDto.java
@@ -0,0 +1,14 @@
+package com.example.Bookstore.dtos;
+
+public record StockDto(
+
+    String name,
+
+    Long book_id,
+
+    int price
+
+
+) {
+
+}
